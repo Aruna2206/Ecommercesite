b86edebb31914f1d004e1c9d51dbd8c279488429
Aruna A <147039151+Aruna2206@users.noreply.github.com>
2025-05-03 21:58:52 +0530

    firstcommit

---
+++ file1.js
@@ -0,0 +1,7 @@
+var sidenav = document.querySelector(".side-nav-list");
+function showNavbar() {
+  sidenav.style.display = "block";
+}
+function hideNavbar() {
+  sidenav.style.display = "none";
+}
